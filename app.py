f9d4b0fc8b89dd50ab724c5e33cb5bd002ddf83d
Rudraksh MK <rudraksh@paperkite.in>
2016-10-21 15:52:40 +0530

    Added a route that takes a timezone ID as a query parameter, and returns a JSON object containing the current UTC time, the corresponding time in the timezone requested, as well as the format string used.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response
 import arrow
 import json
 from timezones import timezones
+import collections
 
 app = Flask(__name__)
 
@@ -10,6 +11,22 @@ def allTimeZones():
     timezone_list = json.dumps(timezones)
     resp = Response(timezone_list, status=200, mimetype='application/json')
     return resp
+
+@app.route('/timezones/convert')
+def convertUtcToTimeZone():
+    timezone_id = int(request.args.get('timezone_id'))
+    for tz in timezones:
+        if tz['timezone_id'] == timezone_id:
+            timezone_name = tz['timezone_name']
+    utc_time = arrow.utcnow()
+    local_time = utc_time.to(timezone_name)
+    payload = collections.OrderedDict()
+    payload['utc'] = utc_time.format('MM-DD-YYYY HH:mm:SS Z')
+    payload[timezone_name.lower()] = local_time.format('MM-DD-YYYY HH:mm:SS Z')
+    payload['format'] = 'MM-DD-YYYY HH:mm:SS Z'
+    resp = Response(json.dumps(payload), status=200, mimetype='application/json')
+    return resp
+
 
 
 if __name__ == '__main__':
